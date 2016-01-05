499bbd5da6c58752418eddf0079ce604d7a03e59
Alexander Blesius <alexander@blesius.eu>
2016-01-05 11:46:26 +0100

    moved Timelords.java to exercise-files and car-related files to garage package

--- src/App.java
+++ src/App.java
@@ -2,24 +2,28 @@ public class App {
 
     public static void main(String[] args) {
 
-        Machine mach1 = new Machine();
+
+
+        /*
+        garage.Machine mach1 = new garage.Machine();
         mach1.start();
         mach1.stop();
 
         System.out.println();
 
-        Car1 car1 = new Car1("Corsa");
+        garage.Car1 car1 = new garage.Car1("Corsa");
         car1.start();
         car1.stop();
 
         System.out.println();
 
-        Car car2 = new Car();
+        garage.Car car2 = new garage.Car();
         car2.setName("Astra");
         car2.show_info();
         car2.start();
         car2.wipe_windshield();
         car2.stop();
+        */
 
     }
 }
